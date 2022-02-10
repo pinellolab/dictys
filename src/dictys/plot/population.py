82463971ef4589f0567badb5b4b4650d9a1ae794
Lingfei Wang <lingfei.wang.github@outlook.com>
2022-02-10 11:04:08 -0500

    Added heatmap for top targets of select TFs.

--- src/dictys/plot/population.py
+++ src/dictys/plot/population.py
@@ -8,18 +8,37 @@ Population level visualization of networks
 def compute_reg_spec(d0,min_entropy=0.5,ncut=0.4,nmin=20,nmax_reg=10,select_state=None):
 	"""
 	Compute state specificity of regulators with out-degree centrality and CPM.
-	d0:  Dataset
-	min_entropy: State specificity entropy level required (relative to random assignment) to select regulator. Lower means more specific.
-	ncut: Minimum probability required
-	nmin: Minimum number of targets required
-	nmax_reg: Maximum TF count for each cell type, selected based on probability
-	Return:
-	n:		Number of targets as np.array(shape=[n_reg,n_state])
-	v:		Degree centrality specificity as np.array(shape=[n_reg,n_state])
-	cpm:	CPM as np.array(shape=[n_reg,n_state])
-	cpm_v:	CPM specificity as np.array(shape=[n_reg,n_state])
-	reg:	Regulators selected from given constraints as np.array
-	reg_s:	Each regulator's specific state corresponding to reg as np.array
+	Specificity is defined as value in this state / sum across all states.
+
+	Parameters
+	----------
+	d0:				dictys.net.network
+		Input network class
+	min_entropy:	float
+		State specificity entropy level required (relative to random assignment) to select regulator. Lower means more specific.
+	ncut:			float
+		Minimum probability required
+	nmin:			int
+		Minimum number of targets required
+	nmax_reg:		int
+		Maximum TF count for each state, selected based on probability
+	select_state:	list of str
+		Only compute specificity among given states
+
+	Returns
+	-------
+	n:		numpy.ndarray(shape=[n_reg,n_state])
+		Number of targets
+	v:		numpy.ndarray(shape=[n_reg,n_state])
+		Degree centrality specificity (value in this state/sum across all states)
+	cpm:	numpy.ndarray(shape=[n_reg,n_state])
+		CPM
+	cpm_v:	numpy.ndarray(shape=[n_reg,n_state])
+		CPM specificity (value in this state/sum across all states)
+	reg:	numpy.ndarray
+		Regulators selected from given constraints
+	reg_s:	numpy.ndarray
+		Each regulator's specific state corresponding to reg
 	"""
 	from dictys.net import stat
 	import numpy as np
@@ -89,11 +108,24 @@ def compute_reg_spec(d0,min_entropy=0.5,ncut=0.4,nmin=20,nmax_reg=10,select_stat
 def fig_heatmap_reg_spec(v,aspect=0.3,figscale=0.15,g_ann=None,**ka):
 	"""
 	Draw heatmap for regulators' state specificity.
-	v:		pandas.DataFrame of state specificity with states as rows and regulator genes as columns
-	aspect:	Aspect ratio of each entry
-	g_ann:	List of regulator genes to annotate
-	ka:		Keyword arguments passed to dictys.plot.heatmap
-	Return:	plt.Figure drawn on
+
+	Parameters
+	----------
+	v:			pandas.DataFrame
+		State specificity with states as rows and regulator genes as columns
+	aspect:		float
+		Aspect ratio of each entry
+	figscale:	float
+		Size scale of figure
+	g_ann:		list of str or None
+		List of regulator genes to annotate
+	ka:			dict
+		Keyword arguments passed to dictys.plot.heatmap
+
+	Returns
+	-------
+	matplotlib.pyplot.Figure
+		Figure drawn
 	"""
 	import numpy as np
 	import matplotlib.pyplot as plt
@@ -116,8 +148,161 @@ def fig_heatmap_reg_spec(v,aspect=0.3,figscale=0.15,g_ann=None,**ka):
 	ax.tick_params(which='both',axis='both',top=True,labeltop=False)
 	return fig1
 
+def fig_heatmap_top(d0,selection,ntop=10,direction=0,gann=[],cmap_value='coolwarm',cmap_type='tab10',color_type=None,normalization='column',aspect=0.2,topheight=0.7,topspace=0.3,figscale=0.15):
+	"""
+	Draw heatmap for top targets of given regulators in given cell types/states.
 
+	Parameters
+	----------
+	d0:				dictys.net.network
+		Input network
+	selection:		list of tuple
+		List of regulator-state pair (or TF-cell type pair) to draw. Each element of list is a tuple (TF, cell type) by name.
+	ntop:			int
+		Number of top targets to draw
+	direction:		int
+		Direction of regulation to be considered for top targets. For -1,0,1, indicates repression,both,activation only.
+	gann:			list of str or str
+		Target genes to annotate. Accepts 'all' for all genes.
+	cmap_value:		str
+		Matplotlib colormap name for heatmap
+	cmap_type:		str
+		Matplotlib colormap name for cell state/type bar. Ignored if color_type is specified.
+	color_type:		dict
+		Dictionary mappping cell state name to bar color.
+	normalization:	str
+		Normalization of regulation strength for heatmap. Accepts:
+		column:	All regulations for each heatmap column are scale normalized to -1 to 1 so the strongest is -1 or 1.
+		none:	No normalization
+	aspect:			float
+		Aspect ratio of heatmap
+	topheight:		float
+		Relative height of top colorbar for cell types/states
+	topspace:		float
+		Relative spacing between colorbar and heatmap
+	figscale:		float
+		Figure size scale
 
+	Returns
+	-------
+	fig:		matplotlib.pyplot.Figure
+		Heatmap drawn
+	colorbar:	matplotlib.pyplot.Figure
+		Colorbar drawn for heatmap
+	data:		pandas.DataFrame
+		Data of heatmap
+	"""
+	from os import linesep
+	import logging
+	import numpy as np
+	import pandas as pd
+	import matplotlib
+	import matplotlib.pyplot as plt
+
+	ndict=[dict(zip(d0.nname[x],range(len(x)))) for x in d0.nids]
+	names=np.array([' '+'-'.join(x) for x in selection])
+	if gann=='all':
+		gann=d0.nname[d0.nids[1]]
+	gann=set(gann)
+
+	#Validations
+	t1=np.nonzero([x[0] not in ndict[0] for x in selection])[0]
+	if len(t1)>0:
+		raise ValueError('TF(s) not found in dataset: '+','.join([selection[x][0] for x in t1]))
+	t1=np.nonzero([x[1] not in d0.sdict for x in selection])[0]
+	if len(t1)>0:
+		raise ValueError('Celltype(s) not found in dataset: '+','.join([selection[x][1] for x in t1]))
+	if direction not in {-1,0,1}:
+		raise ValueError('Parameter direction must be -1, 0, or 1.')
+
+	#Cell type colors
+	color_type=None
+	if color_type is None:
+		if cmap_type is None:
+			raise ValueError('At least one of color_type and cmap_type must be assigned.')
+		typelist=[]
+		for xi in selection:
+			if xi[1] not in typelist:
+				typelist.append(xi[1])
+		t1=plt.get_cmap(cmap_type)
+		if hasattr(t1,'N') and t1.N<len(typelist):
+			raise RuntimeError('Colormap has fewer colors than cell types.')
+		t1=t1(np.linspace(0,1,t1.N)[:len(typelist)])
+		color_type=dict(zip(typelist,t1))
+
+	#Obtain network
+	net=[(ndict[0][x[0]],d0.sdict[x[1]]) for x in selection]
+	net=np.array([d0.prop['es']['w'][x[0],:,x[1]] for x in net])
+	#Keep only requested directions for target gene selection
+	net2=net.copy()
+	if direction==-1:
+		net2*=net2<0
+	elif direction==1:
+		net2*=net2>0
+	net2=np.abs(net2)
+	#Validations
+	t1=(net2!=0).sum(axis=1)
+	t2=np.nonzero(t1==0)[0]
+	if len(t2)>0:
+		raise ValueError('TF-cell type pair(s) have no targets in the specified direction: '+','.join(names[t1]))
+	t2=np.nonzero(t1<ntop)[0]
+	if len(t2)>0:
+		logging.warn('TF-cell type pair(s) have <{} targets in the specified direction: '.format(ntop)+','.join(names[t1]))
+	#Select target genes to show
+	t1=np.argpartition(net2,-ntop,axis=1)[:,-ntop:]
+	t1=np.array([t1[x,(net2 if direction!=0 else net)[x,t1[x]].argsort()[::-1]] for x in range(len(t1))])
+	t2=[]
+	for xi in t1.ravel():
+		if xi not in t2:
+			t2.append(xi)
+	t1=np.array(t2)
+	#Filter network and gene names
+	net=net[:,t1]
+	net2=net2[:,t1]
+	names2=d0.nname[d0.nids[1][t1]]
+	ns=np.array(net.shape)
+	#Normalize network
+	if normalization=='column':
+		net=(net.T/net2.max(axis=1)).T
+	elif normalization is None or normalization=='none':
+		pass
+	else:
+		raise ValueError('Unknown value for parameter normalization')
+	vmax=(net*direction).max() if direction!=0 else np.abs(net).max()
+	
+	#Figure size
+	figsize1=(figscale*ns[0],figscale*aspect*ns[1])
+	figsize2=(figsize1[0],figscale*(topheight+topspace))
+	figsize=(figsize1[0],figsize1[1]+figsize2[1])
+	fig=plt.figure(figsize=figsize)
+	#Panel for cell type color
+	ax=fig.add_axes([0,1-topheight*figscale/figsize[1],1, topheight*figscale/figsize[1]])
+	t1=np.array([color_type[x[1]] for x in selection])
+	t1=t1.reshape(1,*t1.shape)
+	ax.imshow(t1,aspect=topheight);
+	ax.set_xticks(np.arange(ns[0]));
+	ax.set_xticklabels(names,rotation=90);
+	ax.set_yticks([])
+	ax.tick_params(bottom=False,top=False,left=False,labeltop=True,labelbottom=False,length=0);
+	[x.set_visible(False) for x in ax.spines.values()]
+	#Panel for heatmap
+	ax=fig.add_axes([0,0,1, figsize1[1]/figsize[1]],sharex=ax)
+	ax.imshow(net.T,cmap=cmap_value,vmin=-vmax,vmax=vmax,aspect=aspect);
+	gann2=np.nonzero([x in gann for x in names2])[0]
+	ax.set_yticks(gann2)
+	ax.set_yticklabels(names2[gann2])
+	ax.tick_params(bottom=False,top=False,left=True,labeltop=False,labelbottom=False);
+	
+	#Figure for colorbar
+	fig2,ax=plt.subplots(figsize=(0.15, 0.8))
+	norm=matplotlib.colors.Normalize(vmin=-1, vmax=1)
+	fig2.colorbar(matplotlib.cm.ScalarMappable(norm=norm,cmap=cmap_value),cax=ax,orientation='vertical')
+	ax.set_title(f'Relative{linesep}strength',loc='center',pad=10)
+	
+	#Prepare data
+	net=pd.DataFrame(net.T,index=names2,columns=names)
+	
+	return (fig,fig2,net)
 
 
 
